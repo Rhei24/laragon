32a2f18107b071b077cca609969c4c56207a448d
Rey Vincent R. Acosta <acostareyvincent@gmail.com>
2023-07-12 09:44:09 +0800

    starting headlessui darkmodetoggle

--- resources/js/Components/DarkModeToggle.jsx
+++ resources/js/Components/DarkModeToggle.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
 import { useState } from 'react'
+import { Switch } from '@headlessui/react'
 
 export default function DarkModeToggle() {
+  const [enabled, setEnabled] = useState(false)
 
+  return (
+
+      /*
     const [onToggle, setOnToggle] = useState(false);
     
     const toggle = () => {
         setOnToggle((previousState) => !previousState);
     }
+  */
 
-  return (
-    /* switch toggle */
-    /*  */
-    <>
-    <button className="px-6 py-2 bg-green-600 rounded-lg" onClick={toggle}>
-            <span className='text-white'>Test</span>
-    </button>
-    {/* logs current state */}
-    {console.log(onToggle)}
-    </>
+    <Switch
+      checked={enabled}
+      onChange={setEnabled}
+      className={`${
+        enabled ? 'bg-blue-600' : 'bg-gray-200'
+      } relative inline-flex h-6 w-11 items-center rounded-full`}
+    >
+      <span className="sr-only">Enable notifications</span>
+      <span
+        className={`${
+          enabled ? 'translate-x-6' : 'translate-x-1'
+        } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+      />
+    </Switch>
   )
 }
+
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -13,9 +13,8 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="flex items-center justify-center p-8">
-                        <DarkModeToggle/>
-                        </div>
+                        <div className="p-6 text-gray-900 dark:text-gray-100">yupper</div>
+                        <DarkModeToggle />
                     </div>
                 </div>
             </div>
