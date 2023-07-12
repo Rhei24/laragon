b276f9feaefe771c80e8be78d9d6f4145205e438
Rey Vincent R. Acosta <acostareyvincent@gmail.com>
2023-07-12 09:42:44 +0800

    starting headlessui darkmodetoggle

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
+import DarkModeToggle from '@/Components/DarkModeToggle';
 
 export default function Authenticated({ user, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
-    /* test commit */
+    
+
+    /* darkmode switch temporary*/
+    const [toggle, setToggle] = useState('');
+
+    useEffect(() => {
+        const savedToggle = localStorage.getItem('toggle');
+        if (savedToggle) {
+          setToggle(savedToggle);
+        }
+      }, []);
+    
+      const handleDarkModeToggle = () => {
+        {/* true = dark, false = light */}
+        const newToggle = toggle === '' ? 'dark' : '';
+        {/* change const toggle to the new assigned mode */}
+        setToggle(newToggle);
+        {/* .setItem requires 2 parameters  */}
+        localStorage.setItem('toggle', newToggle);
+
+        /* darkmode end */
+
+      };
+
     return (
-        /* darkmode switch temporary*/
-        <div className="dark">
+        /* ! current problem, localstorage stores false but still displays true*/
+        
+        <div className={toggle}>
         <div className="min-h-screen bg-gray-100 dark:bg-gray-900 ">
             {/* ?? */}
             <nav className="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
@@ -24,6 +49,28 @@ export default function Authenticated({ user, header, children }) {
                                 <Link href="/">
                                     <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                                 </Link>
+                                {/* Button for darkmode (put in a separate component)*/}
+                                <>
+                                <button className="px-6 py-2 bg-green-600 rounded-lg" onClick={handleDarkModeToggle}>
+                                {
+                                    toggle === '' ?
+                                    <span className="text-white"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" clipRule="evenodd" />
+</svg>
+</span>
+                                    :
+                                    <span className="text-white"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                    <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+                                  </svg>
+                                  
+                                  </span>
+                                }
+                                    {/*<span className='text-white'>Test</span> */}
+                                </button>
+                                {/* logs current state */}
+                                {console.log(toggle)}
+                                </>
+                                {/* darkmode end */}
                             </div>
 
 
@@ -129,6 +176,7 @@ export default function Authenticated({ user, header, children }) {
 
             <main>{children}</main>
         </div>
+        {/* darkmode end*/}
         </div>
     );
 }
