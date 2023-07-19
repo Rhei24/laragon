968f9aaee9af1d927d2f5971ba5c955c20a2ef36
Rey Vincent R. Acosta <acostareyvincent@gmail.com>
2023-07-19 11:46:24 +0800

    Map.jsx still has deconstruct error

--- resources/js/Components/SideBar.jsx
+++ resources/js/Components/SideBar.jsx
@@ -1,92 +1,101 @@
-import React from 'react'
+import React from "react";
 import {
-    HomeIcon, UserGroupIcon, FolderIcon, CalendarIcon, DocumentDuplicateIcon, ChartPieIcon, Cog6ToothIcon
-} from '@heroicons/react/24/outline'
-import ApplicationLogo from './ApplicationLogo'
-import SideBarButton from './SideBarButton'
+    HomeIcon,
+    UserGroupIcon,
+    FolderIcon,
+    CalendarIcon,
+    DocumentDuplicateIcon,
+    ChartPieIcon,
+    Cog6ToothIcon,
+} from "@heroicons/react/24/outline";
+import ApplicationLogo from "./ApplicationLogo";
+import SideBarButton from "./SideBarButton";
 
 /* text-slate-200(?) hover:text-white */
 /* sticky */
 /* edit so that Content fits in the ResponsiveNavLink */
 
-
 export default function SideBar({}) {
-  return (
-    /* try to send prop of classname='' here */
-    <div className="">
-        {/* Content */}
-        <div className="text-black dark:text-white p-4 sm:bg-blue-700 sm:dark:bg-black rounded-l-lg space-y-4">
+    return (
+        /* try to send prop of classname='' here */
+        <div className="">
+            {/* Content */}
+            <div className="text-black dark:text-white p-4 sm:bg-blue-700 sm:dark:bg-black rounded-l-lg space-y-4">
                 {/* Logo Area */}
                 <div className="hidden sm:flex mb-8 pt-4">
-                    
-                    <ApplicationLogo className="stroke-red-800 fill-red-500 w-12 h-12"/>
+                    <ApplicationLogo className="stroke-red-800 fill-red-500 w-12 h-12" />
                 </div>
-                 {/* Logo end*/}
-            
-        
-        {/* Content */}
-        <div className="flex flex-col gap-y-6 mb-6">
-            {/* Tabs Area */}
-            <div className="flex flex-col gap-y-2">
-            <SideBarButton>
-                <HomeIcon className="w-6 h-6"/>
-                <span>Dashboard</span>
-            </SideBarButton>
-            <SideBarButton>
-                <UserGroupIcon className="w-6 h-6"/>
-                <span>Team</span>
-            </SideBarButton>
-            <SideBarButton>
-                <FolderIcon className="w-6 h-6"/>
-                <span>Projects</span>
-            </SideBarButton>
-            <SideBarButton>
-                <CalendarIcon className="w-6 h-6"/>
-                <span>Calendar</span>
-            </SideBarButton>
-            <SideBarButton>
-                <DocumentDuplicateIcon className="w-6 h-6"/>
-                <span>Documents</span>
-            </SideBarButton>
-            <SideBarButton>
-                <ChartPieIcon className="w-6 h-6"/>
-                <span>Reports</span>
-            </SideBarButton>
+                {/* Logo end*/}
 
-            <SideBarButton>
-                <DocumentDuplicateIcon className="w-6 h-6"/>
-                <span>SideBarButtonComponentImports</span>
-            </SideBarButton>
-            </div>
-            {/* Tabs end */}
-    
-            {/* Teams Area */}
-            <div className="flex flex-col gap-y-2">
-            <h6>Your teams</h6>
-            <SideBarButton>
-                <div className="px-2 py-1 border border-slate-400 bg-slate-300 bg-transparent rounded text-xs bg-opacity-30">H</div>
-                <span className="">HeroIcons</span>
-            </SideBarButton>
+                {/* Content */}
+                <div className="flex flex-col gap-y-6 mb-6">
+                    {/* Tabs Area */}
+                    <div className="flex flex-col gap-y-2">
+                        <SideBarButton>
+                            <HomeIcon className="w-6 h-6" />
+                            <span>Dashboard</span>
+                        </SideBarButton>
+                        <SideBarButton>
+                            <UserGroupIcon className="w-6 h-6" />
+                            <span>Team</span>
+                        </SideBarButton>
+                        <SideBarButton>
+                            <FolderIcon className="w-6 h-6" />
+                            <span>Projects</span>
+                        </SideBarButton>
+                        <SideBarButton>
+                            <CalendarIcon className="w-6 h-6" />
+                            <span>Calendar</span>
+                        </SideBarButton>
+                        <SideBarButton>
+                            <DocumentDuplicateIcon className="w-6 h-6" />
+                            <span>Documents</span>
+                        </SideBarButton>
+                        <SideBarButton>
+                            <ChartPieIcon className="w-6 h-6" />
+                            <span>Reports</span>
+                        </SideBarButton>
 
-            <SideBarButton>
-                <div className="px-2 py-1 border border-slate-400 bg-slate-300 bg-transparent rounded text-xs bg-opacity-30">T</div>
-                <span className="">Tailwind Labs</span>
-            </SideBarButton>
+                        <SideBarButton>
+                            <DocumentDuplicateIcon className="w-6 h-6" />
+                            <span>SideBarButtonComponentImports</span>
+                        </SideBarButton>
+                    </div>
+                    {/* Tabs end */}
 
-            <SideBarButton>
-                <div className="px-2 py-1 border border-slate-400 bg-slate-300 bg-transparent rounded text-xs bg-opacity-30">W</div>
-                <span className="">Workcation</span>
-            </SideBarButton>
+                    {/* Teams Area */}
+                    <div className="flex flex-col gap-y-2">
+                        <h6>Your teams</h6>
+                        <SideBarButton>
+                            <div className="px-2 py-1 border border-slate-400 bg-slate-300 bg-transparent rounded text-xs bg-opacity-30">
+                                H
+                            </div>
+                            <span className="">HeroIcons</span>
+                        </SideBarButton>
+
+                        <SideBarButton>
+                            <div className="px-2 py-1 border border-slate-400 bg-slate-300 bg-transparent rounded text-xs bg-opacity-30">
+                                T
+                            </div>
+                            <span className="">Tailwind Labs</span>
+                        </SideBarButton>
+
+                        <SideBarButton>
+                            <div className="px-2 py-1 border border-slate-400 bg-slate-300 bg-transparent rounded text-xs bg-opacity-30">
+                                W
+                            </div>
+                            <span className="">Workcation</span>
+                        </SideBarButton>
+                    </div>
+                </div>
+
+                {/* Settings */}
+
+                <button className="flex items-center space-x-2 hover:bg-slate-500 w-full focus:bg-slate-500 px-2 py-1 mt-full">
+                    <Cog6ToothIcon className="w-6 h-6" />
+                    <span className="">Settings</span>
+                </button>
             </div>
         </div>
-
-            {/* Settings */}
-
-            <button className="flex items-center space-x-2 hover:bg-slate-500 w-full focus:bg-slate-500 px-2 py-1 mt-full">
-                <Cog6ToothIcon className="w-6 h-6"/>
-                <span className="">Settings</span>
-            </button>
-        </div>
-    </div>
-  )
+    );
 }
--- resources/js/Pages/Map.jsx
+++ resources/js/Pages/Map.jsx
@@ -1,53 +1,106 @@
-import React from 'react';
-import { useState, useRef } from 'react';
-import { GoogleMapsProvider } from '@ubilabs/google-maps-react-hooks';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import React, { useEffect } from "react";
+import { useState, useRef } from "react";
+import {
+    GoogleMapsProvider,
+    useGoogleMap,
+} from "@ubilabs/google-maps-react-hooks";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 
 /* arbitrary values for now */
 /* process has problem, use exact api key atm */
 
 /* default map target (change to my location) */
 const mapOptions = {
-  zoom: 12,
-  center: {
-    lat: 8.483313,
-    lng: 124.644727,
-  },
+    zoom: 12,
+    center: {
+        lat: 8.483313,
+        lng: 124.644727,
+    },
 };
 
 export default function Map({ auth }) {
-  const [mapContainer, setMapContainer] = useState(null);
+    const [mapContainer, setMapContainer] = useState(null);
 
-  return (
-    <AuthenticatedLayout user={auth.user}>
-      {/* Wrapper for GoogleMaps API */}
-      <GoogleMapsProvider
-        googleMapsAPIKey="AIzaSyCiNeQkH8EhM9oqKusqL3K0R_XjOQ4DTmM"
-        mapOptions={mapOptions}
-        mapContainer={mapContainer}
-        
-      >
-        <div ref={(node) => setMapContainer(node)} className="relative h-screen w-screen">
-          <Location /> </div>
-      </GoogleMapsProvider>
-      {/* Wrapper end */}
+    return (
+        <AuthenticatedLayout user={auth.user}>
+            {/* Wrapper for GoogleMaps API */}
+            <GoogleMapsProvider
+                googleMapsAPIKey="AIzaSyCiNeQkH8EhM9oqKusqL3K0R_XjOQ4DTmM"
+                mapOptions={mapOptions}
+                mapContainer={mapContainer}
+            >
+                <div className="relative">
+                    <div
+                        ref={(node) => setMapContainer(node)}
+                        className="h-screen w-screen"
+                    >
+                        {/* initially renders then goes away when google maps loads in */}
+                        {/* first time it renders, no map */}
+                        <Location />
+                    </div>
+                    {/* scam, overlaps the <Location /> above whether it renders or not */}
+                </div>
+            </GoogleMapsProvider>
 
-      <div className="bg-black w-full p-4 text-white text-center">Footer check</div>
-    </AuthenticatedLayout>
-  );
+            <div className="bg-black w-full">
+                <p className="p-4 text-white text-center">Footer check</p>
+            </div>
+
+            {/* Wrapper end */}
+        </AuthenticatedLayout>
+    );
 }
 
-function Location(){
-  const [lat, setLat] = useState(8.483313);
-  const [lng, setLng] = useState(124.644727);
+function Location() {
+    const [lat, setLat] = useState(8.483313);
+    const [lng, setLng] = useState(124.644727);
 
-  return ( 
-  <div className="absolute top-0 right-1/2 space-x-2">
-    <input type="number" value={lat} onChange={(event) => setLat(event.target.value)} />
-    <input type="number" value={lat} onChange={(event) => setLat(event.target.value)} />
-  </div>
+    const { map } = useGoogleMap();
+    const markerRef = useRef();
 
-  )
+    useEffect(() => {
+        /* if theres no map, return since you cant place market when theres no map */
+        if (!map || markerRef.current) return;
+
+        /* use .current to change value of Ref  */
+        /* run only once */
+        markerRef.current = new google.maps.Marker({ map });
+    }, [map]);
+
+    /* do stuff/logs the latitutde and longitude when either/both of their value changes */
+    useEffect(() => {
+        /* CATCH error */
+        /* if theres no marker yet, return */
+        if (!markerRef.current) return;
+        {
+            /* if lat/lng is not a number, invalid request */
+        }
+        if (isNan(lat) || isNan(lng)) return;
+
+        /* if conditions are met, continue */
+        markerRef.current.setPosition({ lat, lng });
+        console.log({ lat, lng });
+    }, [lat, lng, map]);
+
+    return (
+        /* IDK WHY IT SOMETIMES DISSAPEARS */
+        /* GET...net::ERR_NETWORK_CHANGED makes it disappear */
+        /* refresh makes it disappear?? */
+        <div className="absolute top-0 right-1/2 space-x-2">
+            <input
+                type="number"
+                value={lat}
+                onChange={(event) => setLat(event.target.value)}
+                step={0.01}
+            />
+            <input
+                type="number"
+                value={lng}
+                onChange={(event) => setLng(event.target.value)}
+                step={0.01}
+            />
+        </div>
+    );
 }
 
 /* */
